7ff4c9aa187323542d1ae260a85aedbddc2ba310
Artem Yushko <yushko2002@gmail.com>
2023-09-25 10:29:31 -0500

    got evaluation done

--- evaluate/evaluate.py
+++ evaluate/evaluate.py
@@ -16,34 +16,48 @@ class Evaluation(object):
         )
         self.logger = logging.getLogger(__name__)
 
-    def time_place(self, problem, size):
-        """
-        Measures the time it takes to place a bin of specific size
-        """
-        start_time = time.time() # testing the empty placement
-        response = bin_size.place_item(problem, size)
-        end_time = time.time()
-        self.logger.info(f"Execution time for place_item size {size}: {end_time-start_time} seconds")
-        return response
-
     def measure_place(self):
         """
         Measures the average bin placement time
         """
         test_problem = bin_size.new_problem() # creating a new problem for testing purposes
-        times = [-1, 0, 1, 5, 15, 99, 100, 101, 1000, 12500]
-        for t_time in times:
-            self.time_place(test_problem, t_time)
-        return 200
+        sizes = [-1, 0, 1, 5, 15, 99, 100, 101, 1000, 12500] # bin sizes we will test
+        total = 0 # total time to be returned
+        for size in sizes:
+            start_time = time.time() # testing each placement size
+            response = bin_size.place_item(test_problem, size)
+            end_time = time.time()
+            self.logger.info(f"Execution time for place_item size {size}: {end_time-start_time} seconds, response: {response}")
+            total += end_time-start_time
+        return total
     
     def measure_newend(self):
         """
         Measures the average time needed to create and delete problems
         """
-        return
+        times = [0, 1, 2, 5, 10, 15, 50, 100, 1000] # how many bins we will be creating as a part of the test
+        total = 0 # total time to be returned
+        for t_time in times:
+            start_time = time.time() # testing each creation size
+            for i in range(t_time):
+                response: bin_size.new_problem() # create n problems
+            end_time = time.time()
+            self.logger.info(f"Execution time for new_problem {t_time} times: {end_time-start_time} seconds, response: {response}")
+            total += end_time-start_time # save the info
+
+        start_time = time.time() # testing deletion time
+        for instanceId in bin_size.binPackingInstances.keys:
+            response = bin_size.end_problem(instanceId)
+        end_time = time.time()
+        self.logger.info(f"Execution time for deleting all problems: {end_time-start_time} seconds, last response: {response}")
+        total += end_time-start_time # save the info
+        return total
     
     def main(self):
         """
         Runs the program and measures its performance in all tasks
         """
-        return
+        total = self.measure_newend()
+        total += self.measure_place()
+        self.logger.info(f"Total running time for {self.algo_name} evaluation is {total}")
+        return total
