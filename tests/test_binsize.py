de260596cd6d5b4b601152ce575ee3c9aeb2af49
JWin23 <iamkaungthantwin@gmail.com>
2023-09-26 17:14:16 -0500

    added Artem's code to test_binsize

--- tests/test_binsize.py
+++ tests/test_binsize.py
@@ -1,7 +1,81 @@
-import pytest
-from . import binsize
+import unittest
+import json
+import sys
+import os
+current_dir = os.path.dirname(os.path.abspath(__file__)) # parallel import is weird
+parent_dir = os.path.dirname(current_dir) # I know what I am doing, I swear
+sys.path.append(parent_dir)
+from api import bin_size
 
-# pytest functions
+class TestBinPackingAPI(unittest.TestCase):
+
+    def setUp(self):
+        bin_size.app.testing = True  # Set the app to testing mode
+        self.client = bin_size.app.test_client()  # Create a test clien
+
+    def test_new_problem(self):
+        response = self.client.get('/newproblem/100')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('ID' in data)
+        self.assertTrue('bins' in data)
+        self.assertEqual(data['bins'], "")
+        
+        # Store the problem ID for later use
+        self.problem_id = data['ID']
+
+    def test_place_item(self):
+        # Create a new problem
+        response = self.client.get('/newproblem/100')
+        data = json.loads(response.data)
+        problem_id = data['ID']
+
+        # Place an item in the problem
+        response = self.client.get(f'/placeItem/{problem_id}/50')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['ID'], problem_id)
+        self.assertEqual(data['size'], '50')
+        self.assertEqual(data['loc'], 1)  # It should be placed in the first bin
+
+    def test_end_problem(self):
+        # Create a new problem
+        response = self.client.get('/newproblem/100')
+        data = json.loads(response.data)
+        problem_id = data['ID']
+
+        # Place some items in the problem
+        self.client.get(f'/placeItem/{problem_id}/50')
+        self.client.get(f'/placeItem/{problem_id}/40')
+
+        # End the problem
+        response = self.client.get(f'/endproblem/{problem_id}')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['ID'], problem_id)
+        self.assertEqual(data['size'], 90)  # Total size of items placed
+        self.assertEqual(data['items'], 2)  # Number of items placed
+        self.assertEqual(data['count'], 1)  # Number of bins used
+
+    def test_invalid_place_item(self):
+        # Create a new problem
+        response = self.client.get('/newproblem/100')
+        data = json.loads(response.data)
+        problem_id = data['ID']
+
+        # Attempt to place an item that is too large
+        response = self.client.get(f'/placeItem/{problem_id}/150')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('error' in data)
+        self.assertEqual(data['error'], 'Problem ID has already been completed or size of item is too large')
+
+if __name__ == '__main__':
+    unittest.main()
 
 
 ##!!!!! if something breaks and it's been fixed, add a test case
@@ -21,23 +95,23 @@ from . import binsize
 #test 10000 bins with bin of size 1000
 
 
-# different bin sizes from 0 to 1 mil
-sizes = [0, 1, 10, 100, 1000, 10000, 100000000000, 1, 3, 5, 7, 11, 13, 17]
+# # different bin sizes from 0 to 1 mil
+# problems_dic = {}
 
 # def retrieve_id():
-#     new_bin = binsize.new_problem()
+#     new_bin = bin_size.newProblem()
 #     problems_dic['ID'] = new_bin['bins']
 #     return new_bin['ID']
 
 
-def test_new_problem():
-    new_bin = binsize.new_problem(100)
-    assert new_bin['bins'] == ""
+# def test_new_problem():
+#     new_bin = bin_size.newProblem()
+#     assert new_bin['bins'] == ""
 
-#make a test Class, and make a new object everytime?
-#or just one object and reuse it?
+# #make a test Class, and make a new object everytime?
+# #or just one object and reuse it?
 
-#should have a new problem creation
+# #should have a new problem creation
 # def test_item_size_0():
 #     id = retrieve_id()
-#     binsize.place_item()
+#     bin_size.place_item()
